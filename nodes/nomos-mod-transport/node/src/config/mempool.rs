94c70bcab1232ae52e09fa2cdec4ab6c207a6d04
Roman <zajic@zajic.net>
2025-06-05 13:38:52 +0800

    fix: rename to nomos-node-mod

--- nodes/nomos-mod-transport/node/src/config/mempool.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MempoolConfig {
-    pub cl_pool_recovery_path: PathBuf,
-    pub da_pool_recovery_path: PathBuf,
-}
